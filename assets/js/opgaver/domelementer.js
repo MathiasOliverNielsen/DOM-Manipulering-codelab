9bae71a754bc31a3cf05cfb20d7b812c6f1058d0
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-03-16 16:59:30 +0100

    opgave 1

--- assets/js/opgaver/domelementer.js
+++ assets/js/opgaver/domelementer.js
@@ -1,24 +1,21 @@
 /* DOMcelementer */
 
-
-
 /* Opgave 1.1
 Du skal finde elementet med id="redFigure" og console logge det.
 Du skal finde elementet med id="yellowFigure" og console logge det. 
 */
 
 // din kode her
-
-
+let redFigure = document.getElementById('redFigure');
+console.log(redFigure);
+let yellowFigure = document.getElementById('yellowFigure');
+console.log(yellowFigure);
 /* opgave 1.2
 Du skal ændre baggrundsfarve på de to elementer du har fundet i opgave 1.1
 farven på elementerne skal være blå. brug evt. elelement.style.backgroundColor
 */
 
-
 // din kode her
-
-
 
 /* opgave 2.1
 Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
@@ -26,9 +23,7 @@ Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
 - et p med teksten: Jeg har løst opgave 2.1
 */
 
-
 // din kode her
-
 
 // opgave 3 liveHTML lists
 /* opgave 3.1
@@ -36,7 +31,6 @@ Du skal finde alle elementer med klassen purpleFigures og console logge resultat
 */
 
 // din kode her
-
 
 /* opgave 3.2
 Du skal finde alle elementer med klassen purpleFigures og bruge array.from() til at konvertere listen
@@ -46,16 +40,12 @@ array.from beskrivelse HER: https://developer.mozilla.org/en-US/docs/Web/JavaScr
 
 // din kode her
 
-
 /* opgave 3.3
 Brug din array til at ændre h3 elementets, inden i purpleFigures, tekst til RED. du kan bruge
 myElement.children[0].innerHTML til at ændre 1. child elements inner html.
 */
 
-
 // din kode her
-
-
 
 /* opgave 4
 opret DOM elementer i elementet med id opgaveFour, udfra data objectet myData.
@@ -64,14 +54,9 @@ Fyld dine elementers innerHtML eller andre atributter med dine data
 */
 
 const myData = {
-    name: 'tiger',
-    image: 'assets/img/standard_tiger.jpg',
-    description: 'En tiger er et stort, katteagtig rovdyr med orange og sort striber i pelsen og skarpe tænder og kløer.'
+  name: 'tiger',
+  image: 'assets/img/standard_tiger.jpg',
+  description: 'En tiger er et stort, katteagtig rovdyr med orange og sort striber i pelsen og skarpe tænder og kløer.',
 };
 
-
 // din kode her
-
-
-
-
